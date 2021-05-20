378d924a1a21c99d2046f855369f2c092de8f9b7
Federico Grandi <fgrandi30@gmail.com>
2021-05-20 10:28:10 +0200

    fix(used-by): update fxi .replace issue

--- api/github-actions/used-by.ts
+++ api/github-actions/used-by.ts
@@ -7,21 +7,27 @@ const octokit = new Octokit({
 export default async (req: NowRequest, res: NowResponse) => {
   const { action, badge, ...badgeStuff } = req.query
 
-  if (!action) return res.status(400)
+  if (!action || typeof action != 'string') return res.status(400)
   const useShield = badge === 'true'
 
-  const search = await octokit.request('GET /search/code', {
-    q: `${action.replace(/[\.,:;/\\`'"=*!?#$&+^|~<>(){}\[\]]/g, '+')} path:.github/workflows/ language:YAML fork:true`
-  }).catch()
+  const search = await octokit
+    .request('GET /search/code', {
+      q: `${action.replace(
+        /[\.,:;/\\`'"=*!?#$&+^|~<>(){}\[\]]/g, // eslint-disable-line no-useless-escape
+        '+'
+      )} path:.github/workflows/ language:YAML fork:true`
+    })
+    .catch()
 
   if (!useShield) res.send(search.data.total_count)
-  else res.send({
-    'schemaVersion': 1,
-    'label': 'used by',
-    'message': `${search.data.total_count}`,
-    'color': '#2088FF',
-    'namedLogo': 'GitHub Actions',
-    'logoColor': 'white',
-    ...badgeStuff
-  })
+  else
+    res.send({
+      schemaVersion: 1,
+      label: 'used by',
+      message: `${search.data.total_count}`,
+      color: '#2088FF',
+      namedLogo: 'GitHub Actions',
+      logoColor: 'white',
+      ...badgeStuff
+    })
 }
